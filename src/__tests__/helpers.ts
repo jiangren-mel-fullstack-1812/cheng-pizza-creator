56cced95405d2d7d7b9fddf9d9d53c64dc23b715
Cheng Chang <cheng.chang@team.telstra.com>
2019-03-18 16:02:00 +1100

    add e2e tests for product api

--- src/__tests__/helpers.ts
+++ src/__tests__/helpers.ts
@@ -1,8 +1,3 @@
-// Copyright IBM Corp. 2019. All Rights Reserved.
-// Node module: @loopback/example-product
-// This file is licensed under the MIT License.
-// License text available at https://opensource.org/licenses/MIT
-
 import { Product } from '../models/index';
 
 /*
